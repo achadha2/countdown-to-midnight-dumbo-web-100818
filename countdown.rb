5866893489453df8c0aeab39b255ca487adaee62
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-09 19:03:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown
-  x = 10
-while x > 0
-  puts "#{x} is less than 10"
-  x += 1
+  number = 10
+while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+puts "HAPPY NEW YEAR!"
 end
 end
